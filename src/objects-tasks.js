d6317d62764a0d5bee083e75aa8bc657973b2582
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-03-14 11:15:42 -0400

    feat:  add solution for mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,14 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const res = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, val]) => {
+      res[key] = (res[key] || 0) + val;
+    });
+  });
+  return res;
 }
 
 /**
